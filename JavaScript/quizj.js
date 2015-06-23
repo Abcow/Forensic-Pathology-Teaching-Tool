8d039024163204441eb1ec57a481a048827e2cd9
James <jadavis1505@gmail.com>
2015-06-23 17:37:00 +0100

    Quiz System

--- JavaScript/quizj.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-   text = document.getElementById("correct");
-   correct = document.getElementById("true");
-   correct.onclick = function(){
-      text.style.opacity = 1;
-   };
-}
